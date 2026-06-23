f647d3acca023fee1dcad15e73ba893de062f702
D3a7hKn1gh7 <angelpisarev@gmail.com>
2026-06-23 23:59:15 +0300

    Add 4 game levels with increasing difficulty
    Also added level progression

--- OOPScape/OOPScape.cpp
+++ OOPScape/OOPScape.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
 #include "Game.h"
 
-int main(int argc, char* argv[])
+int main()
 {
-    std::string levelFile = "levels/level1.txt";
-    if (argc > 1)
-        levelFile = argv[1];
+    const char* levels[] = {
+        "levels/level1.txt",
+        "levels/level2.txt",
+        "levels/level3.txt",
+        "levels/level4.txt"
+    };
+
+    int totalLevels = 4;
 
     try
     {
-        Game game;
-        game.loadLevel(levelFile);
-        game.run();
+        for (int i = 0; i < totalLevels; ++i)
+        {
+            std::cout << "\n=== Level " << (i + 1) << " ===\n";
+
+            Game game;
+            game.loadLevel(levels[i]);
+            game.run();
+
+            if (!game.hasWon())
+                break;
+
+            if (i < totalLevels - 1)
+                std::cout << "\nLevel " << (i + 1)
+                << " complete! Proceeding to level " << (i + 2) << "...\n";
+            else
+                std::cout << "\n*** Congratulations! You completed all levels! ***\n";
+        }
     }
     catch (const std::exception& e)
     {
